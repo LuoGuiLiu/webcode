2bc462a33df9ef04a29c903ad6ca7420787f89b5
LuoGuiLiu <2529352776@qq.com>
2018-05-18 22:26:41 +0800

    Test

---
+++ work/js/work.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+	var Swidth = document.documentElement.scrollWidth;
+	var Op = document.getElementsByTagName("p")
+	if(Swidth<=900){
+		Op.style.cssTest = "font-size:20px"
+	}
+
+}
